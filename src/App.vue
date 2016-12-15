b495898d4546d6238dafdd2917d11bd0773acc0e
Jason Wen <jason@leadsocial.com>
2016-12-15 19:06:10 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: ['My first quote', 'This is just a quote'],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    'app-quote-grid': QuoteGrid
+  }
+}
+</script>
---
+++ src/components/Quote.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-xs-6">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.quote { background-color:#fafafa; padding:2rem; border:1px solid #ddd;}
+</style>
