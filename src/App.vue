ffc43aee83f4e749ab645bfdd04879e90a3d4d39
Jason Wen <jason@leadsocial.com>
2016-12-16 15:20:10 +0100

    Delete quotes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="col-sm-12 text-center">
+      <div class="alert alert-info">Info: click on a quote to delete it</div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +26,9 @@ export default {
   methods: {
     newQuote(quote) {
       this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   }
 }
